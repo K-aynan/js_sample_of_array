82d41a1f236817855cf4486c3ba85b8f01d3082a
kaynansn11@gmail.com <kaynansn11@gmail.com>
2023-06-11 10:56:32 -0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,22 @@
+var alunos = [
+  {nome: 'Kaynan', nota: 8},
+  {nome: 'Lyan', nota: 9},
+  {nome: 'Caio', nota: 10},
+  {nome: 'Roberta', nota: 5},
+  {nome: 'Leticia', nota: 4},
+  {nome: 'Laara', nota: 9},
+];
+
+function obterAlunosAprovados(arrayDeAlunos) {
+  var alunosAprovados = [];
+
+  for(var i = 0; i < arrayDeAlunos.length; i++) {
+    if(arrayDeAlunos[i].nota >= 6 ) {
+      alunosAprovados.push(arrayDeAlunos[i]);
+    }
+  }
+  return alunosAprovados;
+}
+
+var alunosAprovados = obterAlunosAprovados(alunos);
+console.log(alunosAprovados);
