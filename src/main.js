6dec9c87a3b1d173c6771c89ff19c0a94eefa195
kaynansn11@gmail.com <kaynansn11@gmail.com>
2023-06-14 09:43:59 -0300

    second?

--- src/main.js
+++ src/main.js
@@ -1,3 +1,26 @@
+// var alunos = [
+//   {nome: 'Kaynan', nota: 8},
+//   {nome: 'Lyan', nota: 9},
+//   {nome: 'Caio', nota: 10},
+//   {nome: 'Roberta', nota: 5},
+//   {nome: 'Leticia', nota: 4},
+//   {nome: 'Laara', nota: 9},
+// ];
+
+// function obterAlunosAprovados(arrayDeAlunos) {
+//   var alunosAprovados = [];
+
+//   for(var i = 0; i < arrayDeAlunos.length; i++) {
+//     if(arrayDeAlunos[i].nota >= 6 ) {
+//       alunosAprovados.push(arrayDeAlunos[i]);
+//     }
+//   }
+//   return alunosAprovados;
+// }
+
+// var alunosAprovados = obterAlunosAprovados(alunos);
+// console.log(alunosAprovados);
+
 var alunos = [
   {nome: 'Kaynan', nota: 8},
   {nome: 'Lyan', nota: 9},
@@ -7,16 +30,8 @@ var alunos = [
   {nome: 'Laara', nota: 9},
 ];
 
-function obterAlunosAprovados(arrayDeAlunos) {
-  var alunosAprovados = [];
+var alunosAprovados = alunos.filter(function(aluno) {
+  return aluno.nota >= 6;
+});
 
-  for(var i = 0; i < arrayDeAlunos.length; i++) {
-    if(arrayDeAlunos[i].nota >= 6 ) {
-      alunosAprovados.push(arrayDeAlunos[i]);
-    }
-  }
-  return alunosAprovados;
-}
-
-var alunosAprovados = obterAlunosAprovados(alunos);
 console.log(alunosAprovados);
